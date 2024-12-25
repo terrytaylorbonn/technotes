92e93ea7d5fdbdc14d47310f53cfcc45e4db7bc1
T Taylor <terrytaylorbonn@gmail.com>
2024-12-25 13:12:56 -0500

    fixed url

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://technotes-api-ttbonn.onrender.com' }),
+//    baseQuery: fetchBaseQuery({ baseUrl: 'https://technotes-api-ttbonn.onrender.com' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://technotes-api-2b45.onrender.com/' }),
     tagTypes: ['Note', 'User'],
     endpoints: builder => ({})
 })
