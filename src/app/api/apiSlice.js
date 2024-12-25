96df4d7ea3a4fa4c6d36d2530406bf0bd463668b
T Taylor <terrytaylorbonn@gmail.com>
2024-12-25 13:23:52 -0500

    asdfasdfds

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
 //    baseQuery: fetchBaseQuery({ baseUrl: 'https://technotes-api-ttbonn.onrender.com' }),
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://technotes-api-2b45.onrender.com/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://technotes-api-2b45.onrender.com' }),
     tagTypes: ['Note', 'User'],
     endpoints: builder => ({})
 })
