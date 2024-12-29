50ed9fe85c94bc8fd0b003f66cabf5ebd1e2c308
T Taylor <terrytaylorbonn@gmail.com>
2024-12-29 08:00:10 -0500

    24.1229 0759am

--- src/components/Public.js
+++ src/components/Public.js
@@ -4,14 +4,14 @@ const Public = () => {
     const content = (
         <section className="public">
             <header>
-                <h1>Welcome ch11 24.1228 12:52 to <span className="nowrap">the test site</span></h1>
+                <h1>technotes frontend ch12 24.1229 07:21 <span className="nowrap">test site</span></h1>
             </header>
             <main className="public__main">
-                <p>React (MERN) test site.</p>
+                <p>TT React (MERN) test site.</p>
                 <address className="public__addr">
-                    aaaaaaaaa<br />
-                    aaaaaaaaaaaa<br />
-                    ccccc City, CA 12345<br />
+                    addr1<br />
+                    addr2<br />
+                    city, CA 12345<br />
                     <a href="tel:+15555555555">(555) 555-5555</a>
                 </address>
                 <br />
